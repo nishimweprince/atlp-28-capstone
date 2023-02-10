874a5875641b6ec551dc24a1b3ace44916864864
Nishimwe Prince <princeelysee@gmail.com>
2023-02-10 08:46:24 +0200

    Added animations on responsive nav

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let hamburger_menu = document.querySelectorAll(".hamburger-icon");
 let responsive_nav = document.querySelector(".responsive-nav");
+let close_nav = document.querySelector(".hide-nav");
 
 hamburger_menu[0].addEventListener("click", () => {
 
     hamburger_menu[0].classList.toggle("is-not-active");
     hamburger_menu[1].classList.toggle("is-not-active");
     responsive_nav.classList.toggle("hide-nav");
+    responsive_nav.style.transform = "translateY(0%)";
 
 });
 
@@ -14,5 +16,5 @@ hamburger_menu[1].addEventListener("click", () => {
     hamburger_menu[0].classList.toggle("is-not-active");
     hamburger_menu[1].classList.toggle("is-not-active");
     responsive_nav.classList.toggle("hide-nav");
-
+    close_nav.style.transform = "translateY(-150%)";
 });
