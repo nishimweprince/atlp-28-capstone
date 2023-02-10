db31aa1a4a07b9fabf2363b20012148680b357f6
Nishimwe Prince <princeelysee@gmail.com>
2023-02-10 17:47:58 +0200

    Progress

---
+++ login.js
@@ -0,0 +1,31 @@
+// <---- LOGIN VALIDATIONS ---->
+
+let login_email = document.getElementById("login-email");
+let login_submit = document.getElementById("login-submit");
+let login_password = document.getElementById("login-password");
+let login_form = document.getElementById("login-form");
+let login_email_error = document.getElementById("login-email-error");
+let login_password_error = document.getElementById("login-password-error");
+
+login_submit.addEventListener("click", (e) => {
+    // e.preventDefault();
+    const emailRegex = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)(.[a-z]+)?$/;
+    const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+
+    if (emailRegex.test(login_email.value)) {
+        login_email_error.style.display = "none";
+        login_email.value = "";
+        login_form.action = './dashboard-home.html'
+    }
+
+    else if (nameRegex.test(login_password.value)) {
+        login_password_error.style.display = "block";
+        console.log("error");
+    }
+
+    else {
+        login_email_error.style.display = "block";
+        console.log("error");
+    }
+
+});
