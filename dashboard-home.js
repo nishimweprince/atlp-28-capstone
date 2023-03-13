767d25a16c4fd30ecdf5b4ecb56b3d685d2fa300
Nishimwe Prince <princeelysee@gmail.com>
2023-03-13 21:04:39 +0200

    Added loaders on blog
    - Added a lagging loader when creating blog is taking longer than expected
    - Redirecting to blogs page after successful publication
    - Editing author name in dashboard
    - Submitting only when social media links are valid

--- dashboard-home.js
+++ dashboard-home.js
@@ -3,7 +3,6 @@ if (sessionStorage.getItem("isLoggedIn") !== "true") {
 }
 const url = "https://angry-leotard-frog.cyclic.app/api";
 
-
 // <--- FORM ELEMENTS --->
 
 // SUBMIT BUTTON
@@ -19,9 +18,17 @@ let author_linkedin = document.getElementById("author-linkedin");
 let author_github = document.getElementById("author-github");
 let file = null;
 
+// CREATE BLOG FEEDBACK
+
+const create_blog_error = document.getElementById("create-blog-error");
+const create_blog_success = document.getElementById("create-blog-success");
+const create_blog_loader = document.getElementById("create-blog-loader");
+const validate_error = document.getElementById("validation-error");
+const create_blog_lagging = document.getElementById("create-blog-lagging");
+
 // HANDLING IMAGE UPLOAD
 blog_image.addEventListener("change", (e) => {
- let temp = e.target.files[0];
+  let temp = e.target.files[0];
   const reader = new FileReader();
   reader.readAsDataURL(temp);
   reader.onload = () => {
@@ -32,54 +39,82 @@ blog_image.addEventListener("change", (e) => {
 blog_submit.addEventListener("click", (e) => {
   e.preventDefault();
 
-    let postObj = {
-        title: blog_title.value,
-        body: blog_body.value,
-        image: file,
-        author_name: author_name.value,
-        author_twitter: author_twitter.value,
-        author_linkedin: author_linkedin.value,
-        author_github: author_github.value,
-    };
+  let postObj = {
+    title: blog_title.value,
+    body: blog_body.value,
+    image: file,
+    author_name: author_name.value,
+    author_twitter: author_twitter.value,
+    author_linkedin: author_linkedin.value,
+    author_github: author_github.value,
+  };
 
-    if (validateSocials(author_twitter.value, author_linkedin.value, author_github.value)) {
-        console.log("Valid socials");
-    }
-    else {
-        console.log("Invalid socials");
-    }
+  if (
+    validateSocials(
+      author_twitter.value,
+      author_linkedin.value,
+      author_github.value
+    )
+  ) {
+    console.log("Valid socials");
+    create_blog_loader.style.display = "block";
+    validate_error.style.display = "none";
 
-  const cookie = document.cookie.split('=')[1];
-  console.log(cookie);
+    setTimeout(() => {
+      create_blog_loader.style.display = "none";
+      create_blog_lagging.style.display = "block";
+    }, 5000);
 
-  fetch(`${url}/blogs`, {
+    const cookie = document.cookie.split("=")[1];
+    console.log(cookie);
+
+    fetch(`${url}/blogs`, {
       method: "POST",
-      headers: { "Content-Type": "application/json",
-      "credentials": `${cookie}`
-  },
-      body: JSON.stringify(postObj)
-  })
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+      headers: { "Content-Type": "application/json", credentials: `${cookie}` },
+      body: JSON.stringify(postObj),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        create_blog_loader.style.display = "none";
+        create_blog_success.style.display = "block";
+        create_blog_error.style.display = "none";
+        validate_error.style.display = "none";
+        create_blog_lagging.style.display = "none";
+
+        setTimeout(() => {
+          window.location.href = "/dashboard-blogs.html";
+        }, 1500);
+
+        console.log(data.data);
+      })
+      .catch((err) => {
+        create_blog_loader.style.display = "none";
+        create_blog_success.style.display = "none";
+        create_blog_error.style.display = "block";
+        create_blog_lagging.style.display = "none";
+        validate_error.style.display = "none";
+      });
+  } else {
+    console.log("Invalid socials");
+    validate_error.style.display = "block";
+  }
 });
 
 const validateSocials = (twitter, linkedin, github) => {
-  
-    let linkedinRegex = /^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile)\/([-a-zA-Z0-9]+)\/*/gm;
-    const linkedinValid = linkedinRegex.test(linkedin);
+  let linkedinRegex =
+    /^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile)\/([-a-zA-Z0-9]+)\/*/gm;
+  const linkedinValid = linkedinRegex.test(linkedin);
 
-    let twitterRegex = /(https:\/\/twitter.com\/(?![a-zA-Z0-9_]+\/)([a-zA-Z0-9_]+))/g;
-    const twitterValid = twitterRegex.test(twitter);
+  let twitterRegex =
+    /(https:\/\/twitter.com\/(?![a-zA-Z0-9_]+\/)([a-zA-Z0-9_]+))/g;
+  const twitterValid = twitterRegex.test(twitter);
 
-    let githubRegex = /^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_]{1,25}$/igm;
-    const githubValid = githubRegex.test(github);
+  let githubRegex =
+    /^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_]{1,25}$/gim;
+  const githubValid = githubRegex.test(github);
 
-    return linkedinValid && twitterValid && githubValid;
-
+  return linkedinValid && twitterValid && githubValid;
 };
-
-
 
 //NAVBAR
 
@@ -109,7 +144,7 @@ logout.addEventListener("click", (e) => {
       .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
   });
   sessionStorage.setItem("isLoggedIn", false);
-  console.log("Logged out")
+  console.log("Logged out");
   setTimeout(() => {
     window.location.href = "./login.html";
   }, 1000);
