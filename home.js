d3d930c1317b2a70335b08b5482740de2b5e8bcc
Nishimwe Prince <princeelysee@gmail.com>
2023-03-13 02:04:20 +0200

    Added dynamic loading for blogs on home page
    - Added linking the dynamically loaded page to its blog-page.html
    - Removed hardcoded blogs

--- home.js
+++ home.js
@@ -1,3 +1,20 @@
+import { api_url } from "./index.js";
+
+// WRAP TEXT
+const wrapText = (text) => {
+  let wordCount = 15;
+  let words = text.split(" ");
+  let truncatedText = "";
+  if (words.length > wordCount) {
+      truncatedText = words.slice(0, wordCount).join(" ");
+  }
+  else {
+      truncatedText = words.join(" ");
+  }
+
+  return truncatedText + "...";
+}
+
 // UNIVERSAL VARIABLES
 let boxContainer = document.querySelectorAll(".box-container");
 let boxes = document.querySelectorAll(".box");
@@ -6,11 +23,25 @@ let skillsContainer = document.querySelector(".skills-container");
 let skillBoxes = document.querySelectorAll(".skill-box");
 let dotsContainer = document.querySelectorAll(".carousel-dots");
 
+// BLOGS CONTAINER
+let blogContainer = document.querySelector(".blogs-container");
+
 // CAROUSEL BUTTONS
 let carouselLeft = Array.from(document.querySelectorAll(".carousel-left"));
 let carouselRight = Array.from(document.querySelectorAll(".carousel-right"));
 
 let currentSlide = 0;
+
+// FETCH BLOGS
+
+fetch(`${api_url}/blogs`, {
+  method: "GET",
+})
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    renderBlogs(data.data);
+  })
 
 // CREATE DOTS
 const createDots = (arr) => {
@@ -87,6 +118,52 @@ window.onload = () => {
   Array.from(boxContainer).forEach((box, index) => {
     dotsContainer[index].append(...createDots(box.children));
   });
+};
+
+function loadBlog(id) {
+  localStorage.setItem("blogId", id);
+  window.location.href = "./blog-page.html";
+}
+
+const renderBlogs = (blogs) => {
+
+  for (let i = 0; i < 3; i++) {
+    const blog_box = document.createElement("div");
+    blog_box.classList.add("blog-box");
+    blog_box.classList.add("box");
+
+    console.log(`${blogs[i]._id}`)
+
+    blog_box.innerHTML = `
+
+    <img src=${blogs[i].image} alt="Blog Image">
+
+                    <div class="blog-text">
+                        <h1>
+                            ${wrapText(blogs[i].title)}
+                        </h1>
+                        <p>
+                            ${wrapText(blogs[i].body)}
+                        </p>
+                    </div>
+
+    `
+const blog_cta = document.createElement("div");
+blog_cta.classList.add("blog-cta");
+const blog_cta_link = document.createElement("a");
+blog_cta_link.setAttribute("href", "#");
+blog_cta_link.textContent = "Read More";
+blog_cta_link.addEventListener("click", (e) => {
+  e.preventDefault();
+  loadBlog(blogs[i]._id);
+});
+
+blog_cta.appendChild(blog_cta_link);
+blog_box.appendChild(blog_cta);
+
+    blogContainer.appendChild(blog_box);
+  };
+
 };
 
 export {createDots}
